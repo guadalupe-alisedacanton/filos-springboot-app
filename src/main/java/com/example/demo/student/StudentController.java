90ce0237ef225a84513e5d94b3ae0da8f79b7bcb
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2021-11-01 20:54:56 -0700

    Changed StudentController.java, localhost:8080/home can now be used as homepage.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 //this class will have all the resources for the API
 @RestController
-@RequestMapping(path = "api/v1/student")
+@RequestMapping(path = "/")
 
 public class StudentController {
 
@@ -18,22 +18,27 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
-    @GetMapping
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+    @GetMapping("/getStudents")
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
-    @PostMapping
+    @PostMapping("/addStudent")
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
 
-    @DeleteMapping(path = "{studentId}")
+//    @DeleteMapping(path = "{studentId}")
+    @DeleteMapping("/deleteStudent{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long id) {
         studentService.deleteStudent(id);
     }
-    @PutMapping(path = "{studentId}")
+//    @PutMapping(path = "{studentId}")
+    @PutMapping("/updateStudent{studentId}")
     public void updateStudent(@PathVariable("studentId") Long id, @RequestParam(required=false) String name, @RequestParam(required=false) String email) {
         studentService.updateStudent(id, name, email);
     }
